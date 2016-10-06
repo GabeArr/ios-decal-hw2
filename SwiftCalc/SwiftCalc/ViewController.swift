d2002069d58f68b1885bc1b2d82c6f2362341eeb
Gabriel Arreola <gabriel.arreola@berkeley.edu>
2016-10-05 18:46:14 -0700

    finished hw2

--- SwiftCalc/SwiftCalc/ViewController.swift
+++ SwiftCalc/SwiftCalc/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     // TODO: This looks like a good place to add some data structures.
     //       One data structure is initialized below for reference.
     var someDataStructure: [String] = [""]
+    var numbersAndOperators: [(value: String, type: String)] = []
     
 
     override func viewDidLoad() {
@@ -53,19 +54,61 @@ class ViewController: UIViewController {
     //       Modify this one or create your own.
     func updateResultLabel(_ content: String) {
         print("Update me like one of those PCs")
+        print(content)
+        resultLabel.text = content
     }
     
     
     // TODO: A calculate method with no parameters, scary!
     //       Modify this one or create your own.
     func calculate() -> String {
-        return "0"
+        let num1 = numbersAndOperators[0].0
+        let op = numbersAndOperators[1].0
+        let num2 = numbersAndOperators[2].0
+        var decimal_index = 0
+        var trailing_zeros = 0
+        if (num1.characters.index(of: ".") != nil || num2.characters.index(of: ".") != nil || op == "/") {
+            var result = String(calculate(a: num1, b: num2, operation: op))
+            if (result.characters.count > 7) {
+                let index = result.index(result.startIndex, offsetBy: 7)
+                result = result.substring(to:index)
+            }
+            if (result.characters.index(of: ".") != nil) {
+                for (index, element) in result.characters.enumerated() {
+                    if (decimal_index != 0) {
+                        if (element != "0") {
+                            trailing_zeros = 1
+                        }
+                    }
+                    if (element == ".") {
+                        decimal_index = index
+                    }
+                }
+                if (trailing_zeros == 0) {
+                    let another_index = result.index(result.startIndex, offsetBy: decimal_index)
+                    result = result.substring(to:another_index)
+                }
+            }
+            return result
+        } else {
+            return String(intCalculate(a: Int(num1)!, b: Int(num2)!, operation: op))
+        }
+        //return "0"
     }
     
     // TODO: A simple calculate method for integers.
     //       Modify this one or create your own.
     func intCalculate(a: Int, b:Int, operation: String) -> Int {
         print("Calculation requested for \(a) \(operation) \(b)")
+        if (operation == "+") {
+            return a + b
+        } else if (operation == "-") {
+            return a - b
+        } else if (operation == "*") {
+            return a * b
+        } else if (operation == "/") {
+            return a / b
+        }
         return 0
     }
     
@@ -73,6 +116,16 @@ class ViewController: UIViewController {
     //       Modify this one or create your own.
     func calculate(a: String, b:String, operation: String) -> Double {
         print("Calculation requested for \(a) \(operation) \(b)")
+        if (operation == "+") {
+            return Double(a)! + Double(b)!
+        } else if (operation == "-") {
+            return Double(a)! - Double(b)!
+        } else if (operation == "*") {
+            return Double(a)! * Double(b)!
+        } else if (operation == "/") {
+            return Double(a)! / Double(b)!
+        }
+
         return 0.0
     }
     
@@ -80,17 +133,148 @@ class ViewController: UIViewController {
     func numberPressed(_ sender: CustomButton) {
         guard Int(sender.content) != nil else { return }
         print("The number \(sender.content) was pressed")
+
+        if (numbersAndOperators.isEmpty) {
+            numbersAndOperators.append((value: sender.content, type: "input"))
+            updateResultLabel(sender.content)
+        } else {
+            let lastElem = numbersAndOperators[numbersAndOperators.count - 1]
+            if (lastElem.1 == "operator") {
+                numbersAndOperators.append((value: sender.content, type: "input"))
+                updateResultLabel(sender.content)
+            }else if (lastElem.1 != "calculation" && lastElem.0.characters.count < 7) {
+                numbersAndOperators.removeLast()
+                if (lastElem.0 == "0") {
+                    numbersAndOperators.append((value:sender.content, type: "input"))
+                    updateResultLabel(sender.content)
+                } else {
+                    numbersAndOperators.append((value: lastElem.0 + sender.content, type: "input"))
+                    updateResultLabel(lastElem.0 + sender.content)
+                }
+            }
+        }
         // Fill me in!
     }
     
     // REQUIRED: The responder to an operator button being pressed.
     func operatorPressed(_ sender: CustomButton) {
+        guard String(sender.content) != nil else {return}
+        print("The operation \(sender.content) was pressed")
+        
+        let arithmetic_operations: [String] = ["+", "-", "*", "/"]
+        if (arithmetic_operations.contains(sender.content)) {
+            if (numbersAndOperators.isEmpty) {
+                numbersAndOperators.append((value: "0", type: "input"))
+                numbersAndOperators.append((value: sender.content, type: "operator"))
+            } else if (numbersAndOperators.count == 3) {
+                let calculation = calculate()
+                
+                
+                numbersAndOperators.removeAll()
+                numbersAndOperators.append((value: calculation, type: "calculation"))
+                if (sender.content != "=") {
+                    numbersAndOperators.append((value: sender.content, type: "operator"))
+                }
+                updateResultLabel(calculation)
+            } else if (numbersAndOperators[numbersAndOperators.count - 1].1 == "operator") {
+                numbersAndOperators.removeLast()
+                numbersAndOperators.append((value: sender.content, type: "operator"))
+            } else {
+                if (sender.content != "=") {
+                    numbersAndOperators.append((value: sender.content, type: "operator"))
+                }
+            }
+
+        } else if (sender.content == "C") {
+            numbersAndOperators.removeAll()
+            updateResultLabel("0")
+        } else if (sender.content == "+/-") {
+            var lastElem = numbersAndOperators[numbersAndOperators.count - 1]
+            if (lastElem.1 == "operator") {
+                numbersAndOperators.removeLast()
+                lastElem = numbersAndOperators[numbersAndOperators.count - 1]
+            }
+            let value = String(Int(lastElem.0)! * -1)
+            if (value.characters.count > 7) {
+                return
+            }
+            let type = lastElem.1
+            numbersAndOperators.removeLast()
+            numbersAndOperators.append((value: value, type: type))
+            updateResultLabel(value)
+            
+            
+        } else if (sender.content == "=") {
+            print("what is going on")
+            print(numbersAndOperators.count)
+            for elem in numbersAndOperators {
+                print(elem)
+            }
+            if(numbersAndOperators.isEmpty) {
+                updateResultLabel("0")
+            } else if (numbersAndOperators.count == 3){
+                print("i should be calculating")
+                updateResultLabel(calculate())
+            }
+        }
+        
         // Fill me in!
     }
     
     // REQUIRED: The responder to a number or operator button being pressed.
     func buttonPressed(_ sender: CustomButton) {
-       // Fill me in!
+        guard String(sender.content) != nil else { return }
+        print("The number \(sender.content) was pressed")
+        
+        if (sender.content == "0"){
+            if (!numbersAndOperators.isEmpty) {
+                let lastElem = numbersAndOperators[numbersAndOperators.count - 1]
+                if (lastElem.1 != "calculation" && lastElem.0.characters.count < 7 && lastElem.0 != "0"){
+                    print("first if statement for 0")
+                    numbersAndOperators.removeLast()
+                    numbersAndOperators.append((value: lastElem.0 + sender.content, type: "input"))
+                    updateResultLabel(lastElem.0 + sender.content)
+                } else if (lastElem.1 == "calculation" && lastElem.0.characters.count < 7){
+                    print("second if statement for 0")
+                    numbersAndOperators.removeLast()
+                    numbersAndOperators.append((value: sender.content, type: "input"))
+                    updateResultLabel(sender.content)
+                } else if (lastElem.1 == "operator") {
+                    print("we have an operation boys and girls. lets add 0")
+                    numbersAndOperators.append((value: sender.content, type: "input"))
+                    updateResultLabel(sender.content)
+                }
+            }
+            else {
+                print("I'M ADDING 0")
+                numbersAndOperators.append((value: sender.content, type: "input"))
+                updateResultLabel(sender.content)
+                
+            }
+        } else if (sender.content == ".") {
+            
+        }
+        
+        if (sender.content == ".") {
+            if (numbersAndOperators.isEmpty) {
+                numbersAndOperators.append((value: "0" + sender.content, type: "input"))
+                updateResultLabel("0" + sender.content)
+            } else {
+                let lastElem = numbersAndOperators[numbersAndOperators.count - 1]
+                if (lastElem.1 == "input") {
+                    if (lastElem.0.characters.index(of: ".") != nil) {
+                        return
+                    }
+                    numbersAndOperators.removeLast()
+                    numbersAndOperators.append((value: lastElem.0 + sender.content, type: "input"))
+                    updateResultLabel(lastElem.0 + sender.content)
+                } else if (lastElem.1 == "operator") {
+                    numbersAndOperators.append((value: "0" + sender.content, type: "input"))
+                    updateResultLabel("0" + sender.content)
+                }
+            }
+        }
+               // Fill me in!
     }
     
     // IMPORTANT: Do NOT change any of the code below.
